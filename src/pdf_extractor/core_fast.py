2950ea3a7b92d74d6e38239967610a8b800e67c2
Y9244 <yusuke9244@icloud.com>
2026-05-16 20:59:08 +0900

    add_parameters in get_lines_from_pdf

--- src/pdf_extractor/core_fast.py
+++ src/pdf_extractor/core_fast.py
@@ -10,7 +10,9 @@ def get_lines_from_pdf(
     page_numbers: Optional[List[int]] = None,
     axis_aligned_only: bool = False,
     min_length: Optional[float] = None,
-    include_curves: bool = False
+    include_lines: bool = True,
+    include_rects: bool = True,
+    include_curves: bool = True
 ):# -> Dict[int, np.ndarray]:
     """
     PDFから線分情報を取得する。
@@ -20,6 +22,8 @@ def get_lines_from_pdf(
     :param page_numbers: 抽出したいページのリスト（例: [0, 1]）。Noneの場合は全ページ。
     :param axis_aligned_only: Trueの場合、水平・垂直線のみ抽出する
     :param min_length: 指定した長さ未満の線分を除外する。Noneの場合は除外しない。
+    :param include_lines: Trueの場合、線分エッジも線分として抽出する
+    :param include_rects: Trueの場合、矩形エッジも線分として抽出する
     :param include_curves: Trueの場合、曲線エッジも線分として抽出する
     :return: ページ番号をキー、線分座標 [x0, y0, x1, y1] の numpy配列を値とした辞書
     """
@@ -39,7 +43,7 @@ def get_lines_from_pdf(
             warning_flag = False
             for d in drawings:
                 for item in d["items"]:
-                    if item[0] == 'l':  # line
+                    if include_lines and item[0] == 'l':  # line
                         p0, p1 = item[1], item[2]
                         x0, y0, x1, y1 = p0.x, p0.y, p1.x, p1.y
                         if _line_filter(x0, y0, x1, y1, axis_aligned_only, min_length):
@@ -47,7 +51,7 @@ def get_lines_from_pdf(
                             x1, y1 = x1 * scale, y1 * scale
                             lines.append((x0, y0, x1, y1))
 
-                    elif item[0] == 're':  # rect
+                    elif include_rects and item[0] == 're':  # rect
                         r = item[1]
                         x0, y0, x1, y1 = r.x0, r.y0, r.x1, r.y1
                         if _line_filter(x0, y0, x1, y1, axis_aligned_only, min_length):
@@ -65,7 +69,7 @@ def get_lines_from_pdf(
                                 x1, y1 = x1 * scale, y1 * scale
                                 lines.append((x0, y0, x1, y1))
 
-                    elif item[0] == 'qu':
+                    elif include_rects and item[0] == 'qu':
                         quad = item[1]
                         pts = [quad.ul, quad.ur, quad.lr, quad.ll, quad.ul]
                         for i in range(4):
